9e7a651e343463c2c7457fd584b6b2d3b888dcdb
Sharrrkkk <projectwebmaster3@gmail.com>
2025-12-05 04:26:18 -0400

    refactor(main.js) route change

--- main.js
+++ main.js
@@ -8,23 +8,23 @@ document.getElementById("send").addEventListener("click", async () => {
 
     //https://sharrrkkk.pythonanywhere.com/api
     //https://sharrrkkk.pythonanywhere.com/embedded
-    const res = await fetch("https://sharrrkkk.pythonanywhere.com/api", {
+    const res = await fetch("https://sharrrkkk.pythonanywhere.com/embedded", {
         method: "POST",
         body: formData,
         cache: "no-store"
     });
 
     //embedded method
-    //const data = await res.text();
-    //document.getElementById("container").innerHTML = data;
+    const data = await res.text();
+    document.getElementById("container").innerHTML = data;
                 
     //JSON API method
-    const data = await res.json();
+    /*const data = await res.json();
     
     document.getElementById("filename").textContent = "FileName: " + data.filename;
     document.getElementById("lines").textContent = "Total Lines: " + data.lines;
     document.getElementById("words").textContent = "Total Words: " + data.words;
     document.getElementById("bytes").textContent = "Total Bytes: " + data.bytes;
-    document.getElementById("chars").textContent = "Total Chars: " + data.chars;
+    document.getElementById("chars").textContent = "Total Chars: " + data.chars;*/
 
 });
